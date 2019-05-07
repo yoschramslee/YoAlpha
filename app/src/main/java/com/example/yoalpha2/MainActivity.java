a835550abd259719a26ebdd76c34144ce007dc18
Ethan P <roboguy365@gmail.com>
2019-05-07 09:44:30 -0400

    Add settings menu

--- app/src/main/java/com/example/yoalpha2/MainActivity.java
+++ app/src/main/java/com/example/yoalpha2/MainActivity.java
@@ -75,7 +75,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent settings = new Intent(this, SettingsActivity.class);
+            startActivity(settings);
         }
 
         return super.onOptionsItemSelected(item);
